2905e91d68803c27f2a61fddd7c3a30d1311be4c
venkatesh badarala <venkatesh.B.8.97@gmail.com>
2025-03-15 03:18:56 +0530

    fixing routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import AppRoutes from "./routes/routes";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return <AppRoutes />;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home"; // Ensure paths are correct
-
-import Products from "../pages/Products";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home"; 
+import Products from "../pages/Products"; // Ensure the correct path
 
 const AppRoutes = () => {
   return (
-
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/products" element={<Products />} />
       </Routes>
-
+    </Router>
   );
 };
 
