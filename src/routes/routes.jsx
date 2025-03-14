f3838f7fa9db9c2be607761a496e7f2add96d2bc
venkatesh badarala <venkatesh.B.8.97@gmail.com>
2025-03-15 02:08:54 +0530

    Setup project structure with routing

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home"; // Ensure paths are correct
-import About from "../pages/About";
+import About from "../pages/dashboard";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<dashboard />} />
       </Routes>
     </Router>
   );
