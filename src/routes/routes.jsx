86205de4c1df0f04382283c1687fb6715eac3985
venkatesh badarala <venkatesh.B.8.97@gmail.com>
2025-03-15 02:48:31 +0530

    fixing db json

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home"; // Ensure paths are correct
-import Dashboard from "../pages/Dashboard";
+
 import Products from "../pages/Products";
 
 const AppRoutes = () => {
@@ -8,7 +8,7 @@ const AppRoutes = () => {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/products" element={<Products />} />
       </Routes>
 
